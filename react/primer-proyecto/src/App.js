f454a97f382bf922b1b904cb448b775abda39706
Tu Nombre <u.sanchezm@yahoo.com>
2021-10-09 12:10:42 -0500

    hola mundo en React con Framework ed team y njx

--- react/primer-proyecto/src/App.js
+++ react/primer-proyecto/src/App.js
@@ -1,20 +1,5 @@
-import logo from './logo.svg';
-//import './App.css';
 import "./styles/style.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Diplomado en Desarrollo de Software
-          <h2>Universidad Autonoma del Estado de México</h2>
-        </p>
-    
-      </header>
-    </div>
-  );
-}
+const App = () => <h1>Hola Mundo</h1>
 
 export default App;
