31eef91450297133385f5828d1e4cba83dfe9ba5
Tu Nombre <u.sanchezm@yahoo.com>
2021-10-17 19:50:55 -0500

    Se implementa componente Switch y se cacha error 404

--- react/primer-proyecto/src/App.js
+++ react/primer-proyecto/src/App.js
@@ -1,4 +1,4 @@
-// import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import "./styles/style.scss"
 import Curso from './Curso'
 import Banner from './Banner'
@@ -6,53 +6,23 @@ import { Switch } from "react-router"
 import Formulario from "./Formulario"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
-const cursos = [
-  {
-    "nombre": "Curso Desarrollo",
-    "image": "https://www.uaemex.mx/images/logos/img_logo_uaem18-21.png",
-    "price": "1200",
-    "profesor": "Ulises",
-    "location": "Online"
-  },
-  {
-    "nombre": "Bases De datos",
-    "image": "https://www.uaemex.mx/images/logos/img_logo_uaem18-21.png",
-    "price": "1500",
-    "profesor": "Fatima",
-    "location": "Online"
-  },
-  {
-    "nombre": "Curso Desarrollo",
-    "image": "https://www.uaemex.mx/images/logos/img_logo_uaem18-21.png",
-    "price": "1200",
-    "profesor": "Ulises",
-    "location": "Online"
-  }
-]
-
 const App = () => (
+
   <Router>
-
     <Switch>
-      <Route path="/" exact component={Banner} />
-      <Router path="/cursos" exact component={Curso} />
-      <Router path="/formulario" exact component={Formulario} />
-      <Route component={
-() => (
-  <div className="ed-grid">
-<h1>Error 404</h1>
-<span>Web Page not found!!</span>
-  </div>
-)
-      } />
+    <Route path="/" exact component={Banner} />
+    <Route path="/cursos" exact component={Curso} />
+    <Route path="/formulario" exact component={Formulario} />
+    <Route component={() => (
+      <div className="ed-grid">
+        <h1>Error 404</h1>
+        <span>Web Page not found!!</span>
+      </div>
+    )}/>
     </Switch>
+  </Router>
+  
 
-    </Router>
+)
 
-    )
-
-
-
-
-
-    export default App;
+export default App;
