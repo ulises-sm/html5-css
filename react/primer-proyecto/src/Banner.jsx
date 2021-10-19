f334c2530d8a77a24ac1e4dbd8ab4d24eb0bd9cf
Tu Nombre <u.sanchezm@yahoo.com>
2021-10-19 13:29:21 -0500

    Prueba

--- react/primer-proyecto/src/Banner.jsx
+++ react/primer-proyecto/src/Banner.jsx
@@ -4,15 +4,28 @@ const Banner = () =>(
 <div className="main-banner img-container" id="main-banner">
   <div className="ed-grid lg-grid-6">
     <div className="lg-cols-4 lg-x-2">
-      <img className="main-banner__img" src="https://www.contrareplica.mx/uploads/2020/03/05/normal/646677a3198d85c9f3b3dea0dc788b62.jpg" alt=""/>
+      <img className="main-banner__img" src="https://phantom-elmundo.unidadeditorial.es/adcc473c853c551d96474c24c4e63df7/resize/746/f/webp/assets/multimedia/imagenes/2019/03/11/15523046827096.jpg" alt=""/>
       <div className="main-banner__data s-center">
-        <p className="t2 s-mb-0">Diplomado en Desarrollo de Software Empresarial</p>
-        <p> UAEMex</p>
-        <a href="https://www.google.com" className="button">Botón del banner</a>
+        <p className="t2 s-mb-0">Glofox... Tu compañero de entrenamiento</p>
+        <p> Vive una vida saludable sin importar donde estes</p>
+        <a href="https://www.google.com" className="button">Solicitar Informes</a>
+        <p>
+        Ulises: En mi caso viajo por todo el pais a veces al extranjero, necesitaba 
+  una solucion para no pagar visita a los gimnasios ya que al final era mas costoso
+
+        </p>
       </div>
     </div>
   </div>
+
+  hola
+  <br/>
+  
+
 </div>
+
+
+
 )
 
 export default Banner;
